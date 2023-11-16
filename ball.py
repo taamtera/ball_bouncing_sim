76926623c6e176a292b6c8fd9f1c6f1873728baf
Taam Paramee <taamtera@gmail.com>
2023-11-16 11:33:12 +0700

    bouncing ball oop

--- ball.py
+++ ball.py
@@ -1,35 +1,35 @@
 import turtle
 import random
+class Ball:
+    
+    def __init__(self,canvas_height,canvas_width,ball_radius):
+        self.xpos= random.randint(-1*canvas_width + ball_radius, canvas_width - ball_radius)
+        self.ypos=random.randint(-1*canvas_height + ball_radius, canvas_height - ball_radius)
+        self.vx= random.randint(1, 0.01*canvas_width)
+        self.vy= random.randint(1, 0.01*canvas_height)
+        self.color= (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+        self.ball_radius= ball_radius
+    
+    def draw_circle(self):
+        # draw a circle of radius equals to size at x, y coordinates and paint it with color
+        turtle.penup()
+        turtle.color(self.color)
+        turtle.fillcolor(self.color)
+        turtle.goto(self.xpos,self.ypos)
+        turtle.pendown()
+        turtle.begin_fill()
+        turtle.circle(self.ball_radius)
+        turtle.end_fill()
 
-def draw_circle(color, size, x, y):
-    # draw a circle of radius equals to size at x, y coordinates and paint it with color
-    turtle.penup()
-    turtle.color(color)
-    turtle.fillcolor(color)
-    turtle.goto(x,y)
-    turtle.pendown()
-    turtle.begin_fill()
-    turtle.circle(size)
-    turtle.end_fill()
+    def move_circle(self, canvas_width, canvas_height):
+        # update the x, y coordinates of ball i with velocity in the x (vx) and y (vy) components
+        self.xpos += self.vx
+        self.ypos += self.vy
 
-def move_circle(i, xpos, ypos, vx, vy, canvas_width, canvas_height, ball_radius):
-    # update the x, y coordinates of ball i with velocity in the x (vx) and y (vy) components
-    xpos[i] += vx[i]
-    ypos[i] += vy[i]
+        # if the ball hits the side walls, reverse the vx velocity
+        if abs(self.xpos + self.vx) > (canvas_width - self.ball_radius):
+            self.vx = -self.vx
 
-    # if the ball hits the side walls, reverse the vx velocity
-    if abs(xpos[i] + vx[i]) > (canvas_width - ball_radius):
-        vx[i] = -vx[i]
-
-    # if the ball hits the ceiling or the floor, reverse the vy velocity
-    if abs(ypos[i] + vy[i]) > (canvas_height - ball_radius):
-        vy[i] = -vy[i]
-
-def initilizing(xpos, ypos, vx, vy, ball_color, canvas_width, canvas_height, ball_radius, num_balls):
-    # create random number of balls, num_balls, located at random positions within the canvas; each ball has a random velocity value in the x and y direction and is painted with a random color
-    for i in range(num_balls):
-        xpos.append(random.randint(-1*canvas_width + ball_radius, canvas_width - ball_radius))
-        ypos.append(random.randint(-1*canvas_height + ball_radius, canvas_height - ball_radius))
-        vx.append(random.randint(1, 0.01*canvas_width))
-        vy.append(random.randint(1, 0.01*canvas_height))
-        ball_color.append((random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)))
+        # if the ball hits the ceiling or the floor, reverse the vy velocity
+        if abs(self.ypos + self.vy) > (canvas_height - self.ball_radius):
+            self.vy = -self.vy
--- run_ball.py
+++ run_ball.py
@@ -9,18 +9,14 @@ canvas_width = turtle.screensize()[0]
 canvas_height = turtle.screensize()[1]
 ball_radius = 0.05 * canvas_width
 turtle.colormode(255)
-color_list = []
-xpos = []
-ypos = []
-vx = []
-vy = []
-ball_color = []
-ball.initilizing(xpos, ypos, vx, vy, ball_color, canvas_width, canvas_height, ball_radius, num_balls)
+balls = []
+for i in range(num_balls):
+    balls.append(ball.Ball(canvas_width, canvas_height, ball_radius))
 while (True):
     turtle.clear()
-    for i in range(num_balls):
-        ball.draw_circle(ball_color[i], ball_radius, xpos[i], ypos[i])
-        ball.move_circle(i, xpos, ypos, vx, vy, canvas_width, canvas_height, ball_radius)
+    for i in balls:
+        i.draw_circle()
+        i.move_circle(canvas_width, canvas_height)
     turtle.update()
 
 # hold the window; close it by clicking the window close 'x' mark
